1e11bc3f8268fa72cd9ab7fc38c16dc2939d9353
chandan kumar <chandan.k@pepperfry.com>
2017-10-24 19:23:46 +0530

    angular template

--- angularecom/src/app/app.module.ts
+++ angularecom/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MainContainerComponent } from './main-container/main-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    MainContainerComponent
   ],
   imports: [
     BrowserModule
